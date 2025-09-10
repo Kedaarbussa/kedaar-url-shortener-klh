b6bc6ce4770d6e087aec50b12a607ce33f1dc39d
Bussa Kedaar <kedaarbussa2604@gmail.com>
2025-09-10 11:24:22 +0530

    1

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            URL Shortener Application
           </Title>
 
           <Text
